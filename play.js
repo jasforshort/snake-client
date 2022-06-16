977a0980ad8562dec0d4e6f1713f35e8c5ee8b66
Jas Mann <jaforshort@hotmail.ca>
2022-06-16 22:33:23 +0000

    Added input functionality with closing game

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
-connect(); 
+connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  const stdout = process.stdout;
+  if (key === '\u0003') {
+    stdout.write("Snake game has been closed.\n");
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput(); 
